7a2d27c79a92bbad311b5cd9edba6012c18e4819
sasha <1217435098@qq.com>
2017-02-27 11:34:47 +0800

    shuiku

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),//less编译css文件
+    concat = require('gulp-concat'),//合并
+    clean = require('gulp-clean'),//清理
+    runSequence = require('run-sequence');//顺序执行任务
+
+// 样式
+gulp.task('testLess', function() {
+    gulp.src('src/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(concat('themes.min.css'))
+        .pipe(gulp.dest('dist/css'));
+});
+//html
+gulp.task('html', function() {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist/'));
+});
+// 清理
+gulp.task('clean', function() {
+    return gulp.src(['dist/css'], { read: false })
+        .pipe(clean());
+});
+// 预设任务
+gulp.task('default', function(callback) {
+    runSequence('clean', 'testLess','html', callback);
+});
