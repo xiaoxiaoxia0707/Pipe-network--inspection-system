c72155e6dc4f8274821c5368f268aefdaa74d394
sasha <1217435098@qq.com>
2017-03-09 19:54:22 +0800

    第一改下dist里面的html的路径,以及gulpfile.js加上js

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,15 @@ gulp.task('testLess', function() {
         .pipe(concat('master.min.css'))
         .pipe(gulp.dest('dist/css'));
 });
+// 脚本
+gulp.task('scripts', function() {
+    gulp.src(['src/js/*.js'])
+         .pipe(gulp.dest('dist/js'));
+    gulp.src(['src/js/lib/*.js'])
+         .pipe(gulp.dest('dist/js/lib'));
+    gulp.src(['src/js/lib/bootstrap/*'])
+         .pipe(gulp.dest('dist/js/lib/bootstrap'));
+});
 //html
 gulp.task('html', function() {
     gulp.src('src/*.html')
@@ -24,10 +33,10 @@ gulp.task('html', function() {
 });
 // 清理
 gulp.task('clean', function() {
-    return gulp.src(['dist/css'], { read: false })
+    return gulp.src(['dist/css','dist/js'], { read: false })
         .pipe(clean());
 });
 // 预设任务
 gulp.task('default', function(callback) {
-    runSequence('clean', 'testLess','html', callback);
+    runSequence('clean', 'testLess','scripts','html', callback);
 });
